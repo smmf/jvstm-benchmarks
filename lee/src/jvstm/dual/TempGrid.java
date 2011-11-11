4aff72834db9ad8a4d130275915ffb12e99c2317
Sérgio Miguel Fernandes <sergio.fernandes@ist.utl.pt>
2011-11-11 17:38:16 +0000

    * Made the tempGrid sparse array parallelization-safe, i.e., now I can   parallelize the track-laying algorithm with parallel nested transactions.

--- lee/src/jvstm/dual/TempGrid.java
+++ lee/src/jvstm/dual/TempGrid.java
@@ -1,40 +1,67 @@
 package jvstm.dual;
 
 import jvstm.VBoxInt;
+import static jvstm.UtilUnsafe.UNSAFE;
 
 public class TempGrid {
-	
+    // values needed for sun.misc.Unsafe operations
+    private static int ARRAY_VBOX_2D_BASE_OFFSET = UNSAFE.arrayBaseOffset(VBoxInt[][].class);
+    private static int ARRAY_VBOX_2D_INDEX_SCALE = UNSAFE.arrayIndexScale(VBoxInt[][].class);
+    private static int ARRAY_VBOX_1D_BASE_OFFSET = UNSAFE.arrayBaseOffset(VBoxInt[].class);
+    private static int ARRAY_VBOX_1D_INDEX_SCALE = UNSAFE.arrayIndexScale(VBoxInt[].class);
+
     private VBoxInt[][] grid;
-    private int rowSize;
     private int colSize;
 
     TempGrid(int rows, int cols) {
-        this.rowSize = rows;
         this.colSize = cols;
         // will lazily allocate the required inner arrays
         this.grid = new VBoxInt[rows][];
     }
 
     int get(int x, int y) {
-        VBoxInt[] row = this.grid[x];
-        if (row == null) {
-            return LeeRouter.TEMP_EMPTY;
-        }
-        VBoxInt box = row[y];
-        if (box == null) {
-            return LeeRouter.TEMP_EMPTY;
-        }
-        return box.getInt();
+        return ensureBoxAt(x,y).getInt();
     }
 
     void put(int value, int x, int y) {
-        if (this.grid[x] == null) {
-            this.grid[x] = new VBoxInt[colSize];
-        }
-
-        if (this.grid[x][y] == null) {
-            this.grid[x][y] = new VBoxInt();
-        }
-        this.grid[x][y].putInt(value);
+        ensureBoxAt(x,y).putInt(value);
     }
+
+    private VBoxInt[] ensureRowAt(int x) {
+        VBoxInt[] currentRow = this.grid[x];
+        VBoxInt[] rowToReturn = currentRow;
+        if (currentRow == null) {
+            long offset = (long)ARRAY_VBOX_2D_BASE_OFFSET + x * ARRAY_VBOX_2D_INDEX_SCALE;
+            VBoxInt[] newRow = new VBoxInt[this.colSize];
+            rowToReturn = newRow;
+
+            // loop until either: my CAS succeeds or I've read some rowToReturn
+            while (!UNSAFE.compareAndSwapObject(this.grid, offset, null, rowToReturn)
+                   && ((rowToReturn = this.grid[x]) == null)) {
+                rowToReturn = newRow;
+            }
+        }
+        return rowToReturn;
+    }
+
+    private VBoxInt ensureBoxAt(int x, int y) {
+        VBoxInt[] currentRow = ensureRowAt(x);
+
+        VBoxInt currentBox = currentRow[y];
+        VBoxInt boxToReturn = currentBox;
+        if (currentBox == null) {
+            long offset = (long)ARRAY_VBOX_1D_BASE_OFFSET + y * ARRAY_VBOX_1D_INDEX_SCALE;
+            VBoxInt newBox = new VBoxInt(LeeRouter.TEMP_EMPTY);
+            boxToReturn = newBox;
+
+            // loop until either: my CAS succeeds or I've read some boxToReturn
+            while (!UNSAFE.compareAndSwapObject(currentRow, offset, null, boxToReturn)
+                   && ((boxToReturn = currentRow[y]) == null)) {
+                boxToReturn = newBox;
+            }
+        }
+        return boxToReturn;
+    }
+
+
 }
